55788f2877d5552969b225dcdf69744633424f8b
kyle <keao.chen@outlook.com>
2026-02-13 10:46:44 +1100

    todo: generating a self-signed TLS certificate

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -13,12 +13,14 @@ type templateData struct {
 	CurrentYear int
 	Snippet     models.Snippet
 	Snippets    []models.Snippet
+	Flash       string
 	Form        any
 }
 
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
 		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
